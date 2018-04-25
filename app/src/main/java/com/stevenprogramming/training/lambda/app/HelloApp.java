f0aed17de01de892ebd0eb316b9be05856f4b36e
Mendez <stme@gft.com>
2018-04-25 10:14:11 -0600

    Steven Ricardo Mendez Brenes Add base project

---
+++ app/src/main/java/com/stevenprogramming/training/lambda/app/HelloApp.java
@@ -0,0 +1,11 @@
+package com.stevenprogramming.training.lambda.app;
+
+import com.stevenprogramming.training.lambda.core.HelloCore;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		HelloCore helloCore = new HelloCore();
+		System.out.println(helloCore.getGreeting("Steven From App to Core") + " " + args[0] + " " + args[1]);
+	}
+}
---
+++ core/src/main/java/com/stevenprogramming/training/lambda/core/HelloCore.java
@@ -0,0 +1,8 @@
+package com.stevenprogramming.training.lambda.core;
+
+public class HelloCore {
+
+	public String getGreeting(String name) {
+		return "CORE SAYS >>> Hello " + name ;
+	}
+}
