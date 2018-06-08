3217aeaa27483f1da4624f8a2e89be980a138c7f
Mendez <stme@gft.com>
2018-06-08 15:35:02 -0600

    Steven Mendez update jar

--- core/src/main/java/com/stevenprogramming/training/lambda/core/sample/SampleIntStream.java
+++ core/src/main/java/com/stevenprogramming/training/lambda/core/sample/SampleIntStream.java
@@ -1,53 +1,59 @@
 package com.stevenprogramming.training.lambda.core.sample;
 
+import java.util.IntSummaryStatistics;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import com.stevenprogramming.training.lambda.core.Execute;
 
-public class SampleIntStream implements Execute{
+public class SampleIntStream implements Execute {
 
-	public void printValuesInstStream(Stream values) {
-		System.out.println("Values IntStream>>>>");
-		values.forEach(valor -> System.out.printf("%d::", valor));
-	}
-	
-	public void printValues(int[] values) {
-		System.out.println("Values >>>>");
-		IntStream.of(values).forEach(valor -> System.out.printf("%d::", valor));
-	}
-	
-	public void printCount(int[] values) {
-		System.out.printf("%nCount: %d%n", IntStream.of(values).count());
-	}
-	
-	public void printMin(int[] values) {
-		System.out.printf("Min: %d%n", IntStream.of(values).min().getAsInt());
-	}
-	
-	public void printMax(int[] values) {
-		System.out.printf("Max: %d%n", IntStream.of(values).max().getAsInt());
-	}
-	
-	public void printSum(int[] values) {
-		System.out.printf("Sum: %d%n", IntStream.of(values).sum());
-	}
-	
-	public void printAverage(int[] values) {
-		System.out.printf("Average: %.2f%n", IntStream.of(values).average().getAsDouble());
-	}
-	
-	@Override
-	public void execute() {
-		int[] values = {1,11,16,3,4,8,2,5,19,21};
-		printValuesInstStream(IntStream.range(0, 20).boxed());
-		printValues(values);
-		printCount(values);
-		printMin(values);
-		printMax(values);
-		printSum(values);
-		printAverage(values);
-		
-	}
+    public void printValuesInstStream(Stream values) {
+        System.out.println("Values IntStream>>>>");
+        values.forEach(valor -> System.out.printf("%d::", valor));
+    }
+
+    public void printValues(int[] values) {
+        System.out.println("Values >>>>");
+        IntStream.of(values).forEach(valor -> System.out.printf("%d::", valor));
+    }
+
+    public void printCount(int[] values) {
+        System.out.printf("%nCount: %d%n", IntStream.of(values).count());
+    }
+
+    public void printMin(int[] values) {
+        System.out.printf("Min: %d%n", IntStream.of(values).min().getAsInt());
+    }
+
+    public void printMax(int[] values) {
+        System.out.printf("Max: %d%n", IntStream.of(values).max().getAsInt());
+    }
+
+    public void printSum(int[] values) {
+        System.out.printf("Sum: %d%n", IntStream.of(values).sum());
+    }
+
+    public void printAverage(int[] values) {
+        System.out.printf("Average: %.2f%n", IntStream.of(values).average().getAsDouble());
+    }
+
+    public void printStatistics(int[] values) {
+        IntSummaryStatistics intSummaryStatistics = IntStream.of(values).summaryStatistics();
+        System.out.println("Statistics >>> " + intSummaryStatistics.toString());
+    }
+
+    @Override
+    public void execute() {
+        int[] values = {1, 11, 16, 3, 4, 8, 2, 5, 19, 21};
+        printValuesInstStream(IntStream.range(0, 20).boxed());
+        printValues(values);
+        printCount(values);
+        printMin(values);
+        printMax(values);
+        printSum(values);
+        printAverage(values);
+        printStatistics(values);
+    }
 
 }
